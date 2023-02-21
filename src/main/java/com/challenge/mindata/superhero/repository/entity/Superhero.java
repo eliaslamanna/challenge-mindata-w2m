cbeaa2bb46b681fe9425670683f598899843e5ac
eliaslamanna <eliaslamanna@hotmail.com>
2023-02-21 02:43:00 -0300

    initialization data

--- src/main/java/com/challenge/mindata/superhero/repository/entity/Superhero.java
+++ src/main/java/com/challenge/mindata/superhero/repository/entity/Superhero.java
@@ -3,11 +3,14 @@ package com.challenge.mindata.superhero.repository.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.UUID;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Data
@@ -16,8 +19,8 @@ import java.util.UUID;
 public class Superhero {
 
     @Id
-    @GeneratedValue(generator = "UUID")
-    private UUID id;
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
 
     private String name;
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO SUPERHERO (name, superhero_name) VALUES ('Bruce Wayne', 'Batman')
+INSERT INTO SUPERHERO (name, superhero_name) VALUES ('Peter Parker', 'Spider-Man')
+INSERT INTO SUPERHERO (name, superhero_name) VALUES ('Tony Stark', 'Iron Man')
